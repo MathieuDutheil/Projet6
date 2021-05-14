87c43f49359a3a7727e870eefa38a4334de48b1a
mathieudutheil <mathieudutheil92@gmail.com>
2021-05-14 19:58:45 +0200

    add Cotation model and CotationRepository

---
+++ src/main/java/escalade/mathieudutheil/model/Cotation.java
@@ -0,0 +1,17 @@
+package escalade.mathieudutheil.model;
+
+
+import javax.persistence.*;
+
+@Entity
+    public class Cotation {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name= "cotationid")
+        private Integer cotationId;
+
+        @Column(name="cotation")
+        private String cotation;
+
+}
