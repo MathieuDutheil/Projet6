d72c7a97ec4c6ca96a73fb9e18210f25659052d8
mathieudutheil <mathieudutheil92@gmail.com>
2021-05-23 01:35:17 +0200

    reindentation refactor  table name to respect underscore_case add getter and setter for variables

--- src/main/java/escalade/mathieudutheil/model/Civilite.java
+++ src/main/java/escalade/mathieudutheil/model/Civilite.java
@@ -1,18 +1,33 @@
 package escalade.mathieudutheil.model;
 
-
 import javax.persistence.*;
 
-
 @Entity
+@Table(name = "civilite")
 public class Civilite {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name= "civiliteid")
+    @Column(name = "civilite_id")
     private Integer civiliteId;
 
-    @Column(name="civilite")
+    @Column(name = "civilite")
     private String civilite;
 
+    //Getter and Setter
+    public Integer getCiviliteId() {
+        return civiliteId;
+    }
+
+    public void setCiviliteId(Integer civiliteId) {
+        this.civiliteId = civiliteId;
+    }
+
+    public String getCivilite() {
+        return civilite;
+    }
+
+    public void setCivilite(String civilite) {
+        this.civilite = civilite;
+    }
 }
