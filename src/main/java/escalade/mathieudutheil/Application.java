54c31b91ab532cd0f56b17b895a9ad497e9a040d
mathieudutheil <mathieudutheil92@gmail.com>
2021-05-29 20:46:32 +0200

    test with Spring application to display some entity

--- src/main/java/escalade/mathieudutheil/Application.java
+++ src/main/java/escalade/mathieudutheil/Application.java
@@ -2,7 +2,9 @@ package escalade.mathieudutheil;
 
 
 import escalade.mathieudutheil.model.Civilite;
+import escalade.mathieudutheil.model.Grimpeur;
 import escalade.mathieudutheil.service.CiviliteService;
+import escalade.mathieudutheil.service.GrimpeurService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -16,6 +18,9 @@ public class Application implements CommandLineRunner {
     @Autowired
     private CiviliteService civiliteService;
 
+    @Autowired
+    GrimpeurService grimpeurService;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
@@ -23,9 +28,17 @@ public class Application implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Iterable<Civilite> civilites = civiliteService.getCivilites();
-        civilites.forEach(civilite -> System.out.println(civilite.getCivilite()) );
-        Optional<Civilite> optionalCivilite = civiliteService.getCiviliteById(1);
-        Civilite civiliteId1 = optionalCivilite.get();
-        System.out.println(civiliteId1.getCivilite());
+        civilites.forEach(civilite -> System.out.println(civilite.getCivilite()));
+
+        Optional<Civilite> optCivilite = civiliteService.getCiviliteById(3);
+        Civilite civiliteId3 = optCivilite.get();
+        System.out.println(civiliteId3.getId() + " " + civiliteId3.getCivilite());
+
+        Iterable<Grimpeur> grimpeurs = grimpeurService.getGrimpeurs();
+        grimpeurs.forEach(grimpeur -> System.out.println(grimpeur.getNom() + " " + grimpeur.getPrenom() + " " + grimpeur.getEmail()));
+
+        Optional<Grimpeur> optGrimpeur = grimpeurService.getGrimpeurById(1);
+        Grimpeur grimpeurId1 = optGrimpeur.get();
+        System.out.println(grimpeurId1.getMembreAssociation());
     }
 }
