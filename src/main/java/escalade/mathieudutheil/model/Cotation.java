3e6e35b3461a219f1d454220f9c19e3c7ef0503e
mathieudutheil <mathieudutheil92@gmail.com>
2021-06-12 19:07:38 +0200

    implement @OneToMany relationship between Cotation and Voie add Getter and Setter for that relationship

--- src/main/java/escalade/mathieudutheil/model/Cotation.java
+++ src/main/java/escalade/mathieudutheil/model/Cotation.java
@@ -1,6 +1,8 @@
 package escalade.mathieudutheil.model;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "cotation")
@@ -14,6 +16,15 @@ public class Cotation {
     @Column(name = "cotation")
     private String cotation;
 
+    @OneToMany(
+            fetch = FetchType.LAZY,
+            cascade = {
+                    CascadeType.PERSIST,
+                    CascadeType.MERGE
+            }
+    )
+    @JoinColumn(name = "cotation_id")
+    private List<Voie> voies = new ArrayList<>();
 
     //Getters and Setters
     public Integer getId() {
@@ -30,5 +41,13 @@ public class Cotation {
 
     public void setCotation(String cotation) {
         this.cotation = cotation;
+    }
+
+    public List<Voie> getVoies() {
+        return voies;
+    }
+
+    public void setVoies(List<Voie> voies) {
+        this.voies = voies;
     }
 }
