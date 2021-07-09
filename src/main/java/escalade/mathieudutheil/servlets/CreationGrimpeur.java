554d4de7745ca8125a238127a654b283ae1d350a
mathieudutheil <mathieudutheil92@gmail.com>
2021-07-09 15:26:09 +0200

    plusieurs essai pour afficher toutes les civilites sur le formulaire de création mais ne marche pas pour le moment

--- src/main/java/escalade/mathieudutheil/servlets/CreationGrimpeur.java
+++ src/main/java/escalade/mathieudutheil/servlets/CreationGrimpeur.java
@@ -1,13 +1,23 @@
 package escalade.mathieudutheil.servlets;
 
 import escalade.mathieudutheil.forms.CreationGrimpeurForm;
+import escalade.mathieudutheil.model.Civilite;
 import escalade.mathieudutheil.model.Grimpeur;
+import escalade.mathieudutheil.service.CiviliteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CreationGrimpeur  extends HttpServlet {
     private static final String ATTRIBUTE_GRIMPEUR = "grimpeur";
@@ -16,8 +26,29 @@ public class CreationGrimpeur  extends HttpServlet {
     public static final String VUE_SUCCES = "/WEB-INF/afficherGrimpeur.jsp";
     public static final String VUE_FORM = "/WEB-INF/creerGrimpeur.jsp";
 
+    @Autowired
+    private CiviliteService civiliteService;
+
+    private WebApplicationContext springContext;
+
+    @Override
+    public void init(final ServletConfig config) throws ServletException {
+        super.init(config);
+        springContext = WebApplicationContextUtils.getRequiredWebApplicationContext(config.getServletContext());
+        final AutowireCapableBeanFactory beanFactory = springContext.getAutowireCapableBeanFactory();
+        beanFactory.autowireBean(this);
+    }
+
+
     public void doGet(HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
         /* À la réception d'une requête GET, simple affichage du formulaire */
+
+
+       Iterable<Civilite> listeCivilites = civiliteService.getCivilites();
+        request.setAttribute("listeCivilites", listeCivilites);
+
+
+
         this.getServletContext().getRequestDispatcher( VUE_FORM ).forward( request, response );
     }
 
@@ -25,6 +56,7 @@ public class CreationGrimpeur  extends HttpServlet {
 
         CreationGrimpeurForm form = new CreationGrimpeurForm();
         Grimpeur grimpeur = form.creerGrimpeur(request);
+
 
         request.setAttribute(ATTRIBUTE_GRIMPEUR, grimpeur);
         request.setAttribute(ATTRIBUTE_FORM,form);
